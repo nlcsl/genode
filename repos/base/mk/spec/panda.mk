d3cd85e5d86d73cc35a1ecd6a5dff533fe6d4afd
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2018-04-24 15:22:51 +0200

    base: remove cortex* compiler flags (fix #2787)
    Remove cortex_a* and arm_v7a specifiers and include directories,
    as they are used nowhere.

--- repos/base/mk/spec/panda.mk
+++ repos/base/mk/spec/panda.mk
@@ -1,17 +1,16 @@
 #
 # Enable peripherals of the platform
 #
-SPECS += omap4 usb cortex_a9 panda gpio framebuffer
+SPECS += omap4 usb panda gpio framebuffer
 
 #
 # Pull in CPU specifics
 #
-SPECS += arm_v7a
+SPECS += arm_v7
 
 #
 # Add device parameters to include search path
 #
 REP_INC_DIR += include/spec/panda
 
-include $(BASE_DIR)/mk/spec/cortex_a9.mk
-include $(BASE_DIR)/mk/spec/arm_v7a.mk
+include $(BASE_DIR)/mk/spec/arm_v7.mk
